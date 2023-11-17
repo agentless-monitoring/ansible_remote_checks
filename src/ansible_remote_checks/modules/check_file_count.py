92e553529167d2ade896a6bf46aaf0b388950c89
Alexander <alexander.lex@ams-osram.com>
2023-11-17 13:24:39 +0100

    File count check + Fix process check (#6)
    * Added File Count check
    
    * add option to suppress process output
    
    * check_process - decode output before parsing
    
    * user python instead of python2
    
    ---------
    
    Co-authored-by: David Voit <david.voit@osram-os.com>

---
+++ src/ansible_remote_checks/modules/check_file_count.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python2
+
+from ansible.module_utils.basic import AnsibleModule
+
+import os
+from datetime import datetime
+
+def count_files(path, min_age):
+  current_time = datetime.now()
+  count = 0
+  for f in os.listdir(path):
+    fullpath = os.path.join(path, f)
+    if os.path.isfile(fullpath) is False:
+        continue
+    stats = os.stat(fullpath)
+    mtime=datetime.fromtimestamp(stats.st_mtime)
+    lastchanged_minutes=int((current_time-mtime).total_seconds()/60)
+    if (lastchanged_minutes >= min_age):
+      count = count + 1
+
+  return {"file_count": count}
+
+
+def main():
+  module_args= dict(
+    path = dict(type='str', required=True),
+    min_age = dict(type='int', default=0)
+  )
+
+  module = AnsibleModule(
+    argument_spec=module_args,
+    supports_check_mode=True
+  )
+
+  path = module.params['path']
+  min_age = module.params['min_age']
+  try:
+    result = count_files(path, min_age)
+  except Exception as ex:
+    module.fail_json(msg=str(ex))
+
+  module.exit_json(**result)
+
+if __name__ == "__main__":
+  main()
